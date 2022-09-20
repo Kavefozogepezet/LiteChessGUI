c92e87fcad80966b7294bf2e406921cd40142859
Kavefozogepezet <apostagibalint@gmail.com>
2022-09-21 00:38:17 +0200

    First

---
+++ src/BoardStyle.java
@@ -0,0 +1,65 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.*;
+
+
+public class BoardStyle {
+    private static final File texDir = new File(System.getProperty("user.dir"), "textures");
+    private static final Image[][] imgs = new Image[2][PieceType.Count.ordinal()];
+
+    public static Image getPieceTexture(Piece piece) {
+        return getPieceTexture(piece.side, piece.type);
+    }
+
+    public static Image getPieceTexture(Side side, PieceType pieceType) {
+        return imgs[side.ordinal() - 1][pieceType.ordinal()];
+    }
+
+    public static void loadStyle(String styleName) {
+        JSONParser parser = new JSONParser();
+        File dir = new File(texDir, styleName);
+        File json = new File(dir, dir.getName() + ".json");
+
+        try(
+                var in = new FileReader(json)
+                ) {
+            JSONObject obj = (JSONObject) parser.parse(in);
+
+            String[] sides = { "white", "black" };
+            String[] pieces = { "", "king", "queen", "bishop", "knight", "rook", "pawn" };
+            for(int sideIdx = 0; sideIdx < 2; sideIdx++) {
+                JSONObject sideObj = (JSONObject) obj.get(sides[sideIdx]);
+
+                for(int pieceIdx = 1; pieceIdx < PieceType.Count.ordinal(); pieceIdx++) {
+                    Object piecePathObj = sideObj.get(pieces[pieceIdx]);
+
+                    if(piecePathObj == null)
+                        continue;
+
+                    String piecePath = (String)piecePathObj;
+                    piecePath = piecePath.replace('/', File.separatorChar) + ".png";
+                    File pieceFile = new File(dir, piecePath);
+
+                    LoadImage(sideIdx, pieceIdx, pieceFile);
+                    System.out.println("Loaded " + sides[sideIdx] + ' ' + pieces[pieceIdx] + ": " + pieceFile.getPath());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void LoadImage(int sIdx, int pIdx, File file) {
+        try {
+            imgs[sIdx][pIdx] = ImageIO.read(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BoardStyle.loadStyle("default");
+
+        var window = new JFrame("Lite Chess GUI");
+        window.setSize(new Dimension(600, 400));
+
+        Board board = new Board();
+        setupBoard(board);
+        window.add(board);
+
+        //window.pack();
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    public static void setupBoard(Board board) {
+        board.setPiece(new Square(4, 0), Piece.WKing);
+        board.setPiece(new Square(4, 7), Piece.BKing);
+
+        board.setPiece(new Square(3, 0), Piece.WQueen);
+        board.setPiece(new Square(3, 7), Piece.BQueen);
+
+        board.setPiece(new Square(0, 0), Piece.WRook);
+        board.setPiece(new Square(0, 7), Piece.BRook);
+        board.setPiece(new Square(7, 0), Piece.WRook);
+        board.setPiece(new Square(7, 7), Piece.BRook);
+
+        board.setPiece(new Square(1, 0), Piece.WKnight);
+        board.setPiece(new Square(1, 7), Piece.BKnight);
+        board.setPiece(new Square(6, 0), Piece.WKnight);
+        board.setPiece(new Square(6, 7), Piece.BKnight);
+
+        board.setPiece(new Square(2, 0), Piece.WBishop);
+        board.setPiece(new Square(2, 7), Piece.BBishop);
+        board.setPiece(new Square(5, 0), Piece.WBishop);
+        board.setPiece(new Square(5, 7), Piece.BBishop);
+
+        for(int i = 0; i < Board.BOARD_SIZE; i++) {
+            board.setPiece(new Square(i, 1), Piece.WPawn);
+            board.setPiece(new Square(i, 6), Piece.BPawn);
+        }
+    }
+}
